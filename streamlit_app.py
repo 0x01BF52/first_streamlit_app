df1ba78710d361d83edba3e3f367ab355391f949
Fangyi <yanfangyi@gmail.com>
2022-11-18 15:46:44 +1300

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+from fpdf import FPDF
+import base64
+
 
 streamlit.title('My Parents New Health Diner');
 streamlit.header('Breakfast Menu')
@@ -14,8 +17,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
-from fpdf import FPDF
-import base64
 
 report_text = st.text_input("Report Text")
 
